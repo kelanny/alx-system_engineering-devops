579065c0e4b0e6659bea5bd382c1f55ce73b2fd3
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-01-30 23:04:57 +0000

    Added task 1 python script that export user's data in a csv file.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,11 +6,16 @@ import requests
 from sys import argv
 
 
-def fetch_user_data(employee_id):
+if __name__ == "__main__":
+
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: ./export_to_csv.py <employee_id>")
+        exit(1)
+
+    employee_id = int(argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
     todos_url = f"{base_url}/todos?userId={employee_id}"
-
     try:
         user_response = requests.get(user_url)
         todos_response = requests.get(todos_url)
@@ -18,18 +23,10 @@ def fetch_user_data(employee_id):
         todos_data = todos_response.json()
     except requests.RequestException as e:
         print(f"Error fetching data: {e}")
-        return None, None
 
     if "id" not in user_data:
         print(f"User with ID {employee_id} not found.")
-        return None, None
-
-    return user_data, todos_data
-
-
-def export_to_csv(employee_id, user_data, todos_data):
-    if not user_data or not todos_data:
-        return
+        exit(1)
 
     user_id = user_data["id"]
     username = user_data["username"]
@@ -49,13 +46,3 @@ def export_to_csv(employee_id, user_data, todos_data):
                 'NOT COMPLETED',
                 'TASK_TITLE': task['title']
             })
-
-
-if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: ./export_to_csv.py <employee_id>")
-        exit(1)
-
-    employee_id = int(argv[1])
-    user_data, todos_data = fetch_user_data(employee_id)
-    export_to_csv(employee_id, user_data, todos_data)
