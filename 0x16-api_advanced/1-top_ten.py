111ad23b68e4e1f996799fc0fd09d6d100ece812
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-06-06 13:49:55 +0300

    Queries 10 ten hot subreddits on Reddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit.
+
+    Parameters:
+    subreddit (str): The name of the subreddit (without the '/r/' prefix).
+
+    Returns:
+        None: Prints the titles of the first 10 hot posts
+        None if the subreddit is invalid.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'my-custom-user-agent'}
+    params = {'limit': 10}
+
+    try:
+        response = requests.get(url, headers=headers,
+                                params=params,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
